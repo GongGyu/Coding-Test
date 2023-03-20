1215e0e9982bd04eff7e0b1edbb8ad2a0206db04
rhdrb0915 <rhdrb0915@naver.com>
2023-03-20 20:42:55 +0900

    프로그래머스 두수의합

---
+++ src/programmers/level0/p120804/Main.java
@@ -0,0 +1,16 @@
+package programmers.level0.p120804;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(2,3) );
+    }
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 + num2;
+        return answer;
+    }
+}
