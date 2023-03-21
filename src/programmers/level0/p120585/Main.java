2fe56370eae3febc8ffe49f03269ab7eeb6031d2
rhdrb0915 <rhdrb0915@naver.com>
2023-03-21 21:22:17 +0900

    프로그래머스 머쓱이보다 키 큰 사람

---
+++ src/programmers/level0/p120585/Main.java
@@ -0,0 +1,20 @@
+package programmers.level0.p120585;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = {149, 180, 192, 170};
+        System.out.println(solution.solution(array, 167));
+    }
+}
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > height) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
