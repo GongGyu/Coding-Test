2718da141a310534a0da99e99207555faa49d389
rhdrb0915 <rhdrb0915@naver.com>
2023-03-22 16:44:16 +0900

    프로그래머스 피자나눠먹기(2)

---
+++ src/programmers/level0/p120816/Main.java
@@ -0,0 +1,18 @@
+package programmers.level0.p120816;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if(n % slice == 0) {
+            answer = n / slice;
+        }else {
+            answer = (n / slice) + 1;
+        }
+        return answer;
+    }
+}
